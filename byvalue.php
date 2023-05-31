51cc161a0aa0064c42d94ba88fcbc013aad3fe92
jekshelby <dzahnaf09@gmail.com>
2023-05-31 11:43:22 +0700

    jek

---
+++ byvalue.php
@@ -0,0 +1,9 @@
+<?php
+    function
+    tambahSatu($value){
+    $value = $value + 1;
+    }
+    $a = 5;
+    tambahSatu($a);
+    echo $a;
+?>
---
+++ perkalian.php
@@ -0,0 +1,8 @@
+<?php
+    function perkalian($bil_1,$bil_2)
+    {
+        $hasil = $bil_1 * $bil_2;
+        return $hasil;
+    }
+    echo "5 X 10 = ".perkalian(5,10);
+?>
